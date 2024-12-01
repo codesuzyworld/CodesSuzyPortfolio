f03565c082acec4a4b55f68a755945a5e2d2665e
suzanna8767 <codessuzy@gmail.com>
2024-12-01 02:04:44 -0500

    Trying to get screensizes right

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./hero.module.css"
 
+
 export default function Hero() {
     console.log("Hero component rendering"); 
       return(
         <section className={styles["hero"]}>
+          <div className={styles["hero-container"]}>
+            <div className={styles["hero-text-container"]}>
             <h1 className={styles["hero-title"]}>Hello!</h1>
             <h2 className={styles["hero-subtitle"]}>I'm <span className={styles["hero-subtitle-name"]}>Suzanna Lam</span></h2>
             <div className={styles["role-container"]}>
@@ -12,6 +15,10 @@ export default function Hero() {
                 <p className={styles["role-pink"]}>Graphics Designer</p>
             </div>
             <p className={styles["hero-description"]}> I specialize in crafting compelling brand identities and intuitive websites. My expertise extends to 2D and 3D design that add depth and engagement to digital experiences.</p>
+            </div>
+
+          </div>
+
         </section>
       )
     }
